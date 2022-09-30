ffce8ddfc5734d81b6edfa47540cb128939d5e36
ewa1do <this.eduardovera@gmail.com>
2022-09-29 20:20:04 -0400

    :lipstick: Estilos del buttonComponent

---
+++ animals/src/components/buttons/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  align-items: center;
+  background-color: #1c6758;
+  border-radius: 6px;
+  color: #eef2e6;
+  display: flex;
+  flex-direction: ${(props) => (props.iconAlignment === 'left' ? 'row' : 'row-reverse')};
+  justify-content: center;
+  max-height: 2rem;
+  max-width: 6em;
+  min-width: 4rem;
+  transition: all 0.3s ease;
+
+  &:hover {
+    filter: brightness(90%);
+  }
+`;
+
+export const ButtonValue = styled.span`
+  margin: 0 4px;
+`;
