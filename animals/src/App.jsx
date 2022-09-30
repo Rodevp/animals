1137efe555e8b10c08e08c30292492f4faa94758
ewa1do <this.eduardovera@gmail.com>
2022-09-29 20:29:36 -0400

    :poop: Probando ButtonComponent en el App :p

--- animals/src/App.jsx
+++ animals/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import { IoHome } from 'react-icons/io5';
+import styled from 'styled-components';
+
+import { ButtonComponent } from './components';
+
+export const Title = styled.h1`
+  font-size: 6em;
+  color: lightseagreen;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Title>Hello, World!</Title>
+      <ButtonComponent
+        value='Home'
+        Icon={<IoHome />}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
