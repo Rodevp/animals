ec48f8cb07fc39792c5627e0418b5834d5a885fc
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-04 20:00:37 -0300

    :recycle: Creando la carpetas con los archivos PopUpMenu

---
+++ animals/src/components/pop-up-menu/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const PopUpMenuContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  background-color: #3d8361;
+  height: 90px;
+  width: 190px;
+  border-radius: 6px;
+  color: #eef2e6;
+  font-weight: 400;
+  font-size: 16px;
+`;
+
+export const LinkStyles = styled.p`
+  display: flex;
+  margin: 0;
+  align-content: center;
+  align-items: center;
+`;
+
+export const IconMenuAlignment = styled.i`
+  margin: 0 4px;
+  display: flex;
+  align-items: center;
+`;
