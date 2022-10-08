6bde07ca88895e6faa5e8bd9388e931008a2d9b9
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-07 23:08:43 -0300

    :poop: AÃIncluyendo Custom Hook que renderiza el PopUpMenu

--- animals/src/components/header/Header.jsx
+++ animals/src/components/header/Header.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import { IoPaw, IoPerson } from 'react-icons/io5';
 import { HeaderContainer, HeaderTitle, IconHeaderAlignment } from './styles.js';
 
 export const HeaderComponent = () => {
+  const [popUpMenuRender, setPopupMenuRender] = useState(false);
   return (
     <HeaderContainer>
       <IconHeaderAlignment>
---
+++ animals/src/hooks/usePopMenuRender.js
@@ -0,0 +1,3 @@
+export const usePopMenuRender = (isRendered) => {
+  return { isRendered };
+};
