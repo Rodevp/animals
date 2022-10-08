13a52227751cf451273607fbe1b10776874ca28b
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-07 22:22:14 -0300

    :zap: Todos los estilos del Header agregados en header/styles e index.html

---
+++ animals/src/components/header/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 12px;
+  background-color: #1c6758;
+  color: #eef2e6;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.p`
+  font-family: 'Indie Flower', cursive;
+  font-size: 32px;
+`;
+
+export const IconMenuAlignment = styled.i`
+  margin: 0 4px;
+  display: flex;
+  height: 32px;
+  align-items: center;
+  color: #eef2e6;
+`;
