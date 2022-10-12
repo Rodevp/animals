1b8724550a72c5500d838dcb1bc87fef23c24d05
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-12 19:32:59 -0300

    Eliminando material innecesario del componente Header

--- animals/src/components/header/Header.jsx
+++ animals/src/components/header/Header.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import { IoPaw, IoPerson } from 'react-icons/io5';
 import { HeaderContainer, HeaderTitle, IconHeaderAlignment } from './styles.js';
-import { usePopMenuRender } from '../../hooks/usePopMenuRender';
 
 export const HeaderComponent = () => {
-  const [popUpMenuRender, setPopupMenuRender] = useState(false);
   return (
     <HeaderContainer>
       <IconHeaderAlignment>
@@ -12,12 +9,7 @@ export const HeaderComponent = () => {
       </IconHeaderAlignment>
       <HeaderTitle>Puppyfy</HeaderTitle>
       <IconHeaderAlignment>
-        <IoPerson
-          onClick={() => {
-            setPopupMenuRender(!popUpMenuRender);
-            usePopMenuRender(!popUpMenuRender);
-          }}
-        />
+        <IoPerson onClick={() => {}} />
       </IconHeaderAlignment>
     </HeaderContainer>
   );
