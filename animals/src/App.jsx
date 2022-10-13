37c22fedaff21cf621d2d1c0fe5de996bb7cda50
ewa1do <this.eduardovera@gmail.com>
2022-10-13 14:20:05 -0400

    :art: preparando rutas y paginas para el HomePage Component

--- animals/src/App.jsx
+++ animals/src/App.jsx
@@ -1,21 +1,7 @@
-import './App.css';
-import { IoHome } from 'react-icons/io5';
-import styled from 'styled-components';
-import { ButtonComponent, HeaderComponent, PopupMenu } from './components';
-
-export const Title = styled.h1`
-  font-size: 6em;
-  color: lightseagreen;
-`;
+import { AppRouter } from './routes/';
 
 function App() {
-  return (
-    <>
-      <Title>Hello, World!</Title>
-      <ButtonComponent value='Home' Icon={<IoHome />} />
-      <HeaderComponent />
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
---
+++ animals/src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const HomePage = () => {
+  return (
+    <>
+      {/* Header Component */}
+
+      {/* Puppy Component */}
+
+      <h1>HomePage</h1>
+    </>
+  );
+};
