2d0937d4ea2c44403970191afa35788fa7c42142
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-15 01:47:49 -0300

    :recycle: Estilos del body card y card header implementados

--- animals/src/pages/home/components/card/header-card/CardHeader.jsx
+++ animals/src/pages/home/components/card/header-card/CardHeader.jsx
@@ -1,3 +1,5 @@
+import { CardHeaderContainer } from './styles';
+
 const CardHeader = () => {
   return <div>CardHeader</div>;
 };
--- animals/src/pages/home/components/card/header-card/styles.js
+++ animals/src/pages/home/components/card/header-card/styles.js
@@ -1,1 +1,22 @@
 import styled from 'styled-components';
+
+export const CardHeaderContainer = styled.div`
+  background-color: #3d8361;
+  color: #ffffff;
+  font-family: 'Indie Flower', cursive;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  padding: 15px;
+`;
+
+export const PetName = styled.p`
+  font-size: 36px;
+`;
+
+export const PetProps = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+  font-family: 'Courier New', Courier, monospace;
+`;
