8233c8aca651f8e84e23dcb8ee51d312c77cc917
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-24 21:43:16 -0300

    :poop: Agregué un nuevo comentario que deber ser modificado

--- animals/src/pages/home/components/header-card/CardHeader.jsx
+++ animals/src/pages/home/components/header-card/CardHeader.jsx
@@ -6,6 +6,8 @@ export function CardHeader() {
       <PetName>Panchito</PetName>
       <PetProps>3 meses</PetProps>
       {/* Acá falta agregar los icons con alguna suerte de conditional rendering */}
+      {/* El problema es que me va a venir del back, un "female"? directamente el icono? 
+      who knows */}
     </CardHeaderContainer>
   );
 }
