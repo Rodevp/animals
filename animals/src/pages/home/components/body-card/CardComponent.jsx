70aed5f43f2199fb59150473b1d769824b6a7b0a
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-25 23:32:47 -0300

    :zap: Pasando los datos a todos los componentes de la card

--- animals/src/pages/home/components/body-card/CardComponent.jsx
+++ animals/src/pages/home/components/body-card/CardComponent.jsx
@@ -3,14 +3,14 @@ import { CardHeader } from '../header-card';
 import { PetImage } from '../image-card';
 import { AdoptButton } from '../button-card';
 
-const CardComponent = () => {
+const CardComponent = (props) => {
   return (
     // La card fue terminada pero es preciso fijarse bien como vamos
     // a colocar los datos que vengan del array u objeto
     <CardContainer>
-      <CardHeader />
-      <PetImage />
-      <AdoptButton />
+      <CardHeader name={props.name} gender={props.gender} />
+      <PetImage imageSRC={props.image} description={props.description} />
+      <AdoptButton name={props.name} />
     </CardContainer>
   );
 };
