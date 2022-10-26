5690916039659a108e16ea29ff3c7ea060767ac9
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-25 23:33:51 -0300

    :sparkles: Agregando el nombre a cada botoóde adopción

--- animals/src/pages/home/components/button-card/AdoptButton.jsx
+++ animals/src/pages/home/components/button-card/AdoptButton.jsx
@@ -1,10 +1,10 @@
 import { AdoptButtonContainer, IconAlignment } from './styles';
 import { IoPaw } from 'react-icons/io5';
 
-export function AdoptButton() {
+export function AdoptButton({ name }) {
   return (
     <AdoptButtonContainer>
-      Adopta a Panchito
+      Adopta a {name}
       <IconAlignment>
         <IoPaw />
       </IconAlignment>
