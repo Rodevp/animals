06e64fee56b3f37dde209078d439321316335966
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-25 23:14:19 -0300

    :recycle: Pequeño comentario previamente añadido

--- animals/src/pages/home/components/body-card/CardComponent.jsx
+++ animals/src/pages/home/components/body-card/CardComponent.jsx
@@ -5,6 +5,8 @@ import { AdoptButton } from '../button-card';
 
 const CardComponent = () => {
   return (
+    // La card fue terminada pero es preciso fijarse bien como vamos
+    // a colocar los datos que vengan del array u objeto
     <CardContainer>
       <CardHeader />
       <PetImage />
