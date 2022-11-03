ae91e4e82856cca007efa396bdda590f15a1125c
Lucio Vasquez <castrocociolucio@gmail.com>
2022-11-03 15:21:59 -0300

    :lipstick: Correción en el tamaño del header principal

--- animals/src/components/header/styles.js
+++ animals/src/components/header/styles.js
@@ -10,11 +10,19 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   position: relative;
+
+  @media screen and (min-width: 800px) {
+    height: 100px;
+  }
 `;
 
 export const HeaderTitle = styled.p`
   font-family: 'Indie Flower', cursive;
   font-size: 32px;
+
+  @media screen and (min-width: 800px) {
+    font-size: 48px;
+  }
 `;
 
 export const IconHeaderAlignment = styled.i`
@@ -23,4 +31,8 @@ export const IconHeaderAlignment = styled.i`
   font-size: 30px;
   align-items: center;
   color: #eef2e6;
+
+  @media screen and (min-width: 800px) {
+    font-size: 48px;
+  }
 `;
