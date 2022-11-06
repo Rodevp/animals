b9b5e9d663e848f95bf83830ef4789414e71f885
Lucio Vasquez <castrocociolucio@gmail.com>
2022-11-06 02:05:52 -0300

    :recycle: Modificando estilos de header para que se acomode mejor en la página

--- animals/src/pages/login/components/header/styles.js
+++ animals/src/pages/login/components/header/styles.js
@@ -4,9 +4,10 @@ export const LoginHeaderContainer = styled.div`
   color: #fff;
   font-family: 'Indie Flower', cursive;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 30px auto;
+  padding: 10px 0;
 `;
 
 export const HeaderTitle = styled.p`
@@ -15,6 +16,8 @@ export const HeaderTitle = styled.p`
 `;
 
 export const AppDescription = styled.p`
+  margin: 10px 0;
   font-size: 24px;
   text-align: center;
+  padding: 0 80px;
 `;
