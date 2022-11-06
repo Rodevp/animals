bbf280bff5d98337ab9fb793e5164b5b5cf63cc3
Lucio Vasquez <castrocociolucio@gmail.com>
2022-11-06 01:52:31 -0300

    :lipstick: Agregando los estilos del header del login

---
+++ animals/src/pages/login/components/header/LoginHeader.jsx
@@ -0,0 +1,10 @@
+import { LoginHeaderContainer, HeaderTitle, AppDescription } from './styles';
+
+export function LoginHeader() {
+  return (
+    <LoginHeaderContainer>
+      <HeaderTitle>Puppify</HeaderTitle>
+      <AppDescription>Conectando mascotas con gente de calidad</AppDescription>
+    </LoginHeaderContainer>
+  );
+}
---
+++ animals/src/pages/login/components/header/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const LoginHeaderContainer = styled.div`
+  color: #fff;
+  font-family: 'Indie Flower', cursive;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 30px auto;
+`;
+
+export const HeaderTitle = styled.p`
+  font-size: 70px;
+  text-align: center;
+`;
+
+export const AppDescription = styled.p`
+  font-size: 24px;
+  text-align: center;
+`;
