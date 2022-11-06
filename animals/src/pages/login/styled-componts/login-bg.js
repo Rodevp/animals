b0476ce96ef49a6cc561473527dde2fadcc676a0
Lucio Vasquez <castrocociolucio@gmail.com>
2022-11-06 01:59:55 -0300

    :recycle: Descomentando CSS del login-bg

--- animals/src/pages/login/styled-componts/login-bg.js
+++ animals/src/pages/login/styled-componts/login-bg.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const LoginContainer = styled.div`
   background-color: #222222;
   color: #fff;
-  /* width: 100%;
+  width: 100%;
   height: 100%;
   min-width: 100%;
   min-height: 100%;
-  background-image: url('');
+  /* background-image: url('');
   background-size: cover;
   background-repeat: no-repeat; */
 `;
