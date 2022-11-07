db81c77acbacde78758a1644015279280f302eac
Lucio Vasquez <castrocociolucio@gmail.com>
2022-11-07 17:15:16 -0300

    :lipstick: Corregidos los estilos del bg del login

--- animals/src/pages/login/styled-componts/login-bg.js
+++ animals/src/pages/login/styled-componts/login-bg.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const LoginContainer = styled.div`
   background-color: #222222;
   color: #fff;
-  width: 100%;
-  height: 100%;
-  min-width: 100%;
-  min-height: 100%;
+  width: 100vw;
+  height: 100vh;
+  min-width: 100vw;
+  min-height: 100vh;
   /* background-image: url('');
   background-size: cover;
   background-repeat: no-repeat; */
